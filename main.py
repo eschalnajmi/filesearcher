f570dcf4ae8e5d1edf6fcb0aa24adc3ed7e69c29
eschalnajmi <eschal.najmi@gmail.com>
2024-11-20 14:02:37 +0000

    feat: fully functional

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import sys
 import os
 import shutil
+import pathlib
 
-def movefiles(sourcedir,destdir,count,convention, excn5, excomezarr):
-    alldestinations = []
-    alldestpaths = []
-    unfound = []
-    exclude = []
-    if excn5:
-        exclude.append('.n5')
-    if excomezarr:
-        exclude.append('.ome.zarr')
-    for dirpath, dirnames, files in os.walk(destdir, topdown=True):
-        dirnames[:] = [d for d in dirnames if d not in exclude]
-        if os.path.isdir(os.path.join(dirpath)):
-            print(f"Found {dirpath}")
-            alldestpaths.append(dirpath)
-            alldestinations.append(os.path.basename(dirpath))
+# [name, path, type, size]
 
-    for file in os.listdir(sourcedir):
-        standard = file[0:count]
-        foldername = standard+convention
-        if foldername in alldestinations:
-            print(f"Moving {file} to {foldername}")
-            if os.path.exists(os.path.join(alldestpaths[alldestinations.index(foldername)], file)):
-                shutil.copy(os.path.join(sourcedir, file), os.path.join(sourcedir, file.split(".")[0]+'_copy'+ file.strip(file.split(".")[0])+file[-1]))
-                shutil.copy(os.path.join(sourcedir, file.split(".")[0]+'_copy'+ file.strip(file.split(".")[0])+file[-1]), alldestpaths[alldestinations.index(foldername)])
-                continue
-            shutil.copy(os.path.join(sourcedir, file), alldestpaths[alldestinations.index(foldername)])
-        else:
-            print(f"Destination folder for {file} not found")
-            unfound.append(file)
+def find(name, directory):
+    exclude = ['.n5', '.ome.zarr', '.ome.tiff', '.zarr']
+    found_files = []
 
-    return unfound
+    for dirpath, dirnames, files in os.walk(directory, topdown=True):
+        # Collect directories in the exclude list and skip them
+        for dirname in dirnames[:]:
+            if any(dirname.endswith(suffix) for suffix in exclude):
+                if name in dirname:
+                    full_path = os.path.join(dirpath, dirname)
+                    found_files.append([dirname, full_path, pathlib.Path(full_path).suffix, "--"])
+                    print(f"Found pyramidal directory: {full_path}")
+                dirnames.remove(dirname)
+
+        # Find files containing the name variable in the filename
+        for file in files:
+            if name in file:
+                full_path = os.path.join(dirpath, file)
+                found_files.append([file, full_path, pathlib.Path(full_path).suffix, os.path.getsize(full_path)])
+                print(f"Found file: {full_path}")
+
+    return found_files
+
+def write_csv(name, found_files):
+    with open(f'{name}.csv', 'w') as f:
+        f.write('NAME,PATH,TYPE,SIZE\n')
+        for file in found_files:
+            f.write(f'{file[0]},{file[1]},{file[2]},{file[3]}\n')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        directory = os.getcwd()
+    else:
+        directory = sys.argv[2]
+
+    name = sys.argv[1]
+    found_files = find(name, directory)
+    write_csv(name, found_files)
